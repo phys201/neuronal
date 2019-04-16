01a7f71d4f2aaf009e4cc64ca67ba7114bce485b
Amelia Paine <apaine@g.harvard.edu>
2019-04-15 20:16:41 -0400

    Added setup config information

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='neuronal',
+      version='0.1',
+      description='Bayesian inference on neuronal signals',
+      url='https://github.com/phys201/neuronal',
+      author='Amelia Paine, Han Sae Jung',
+      author_email='apaine@g.harvard.edu, hansaejung@g.harvard.edu',
+      license='GPL',
+      packages=['neuronal'],
+      zip_safe=False)
