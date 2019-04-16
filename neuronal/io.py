ab8a3f23045f148d17ff1973bb39c979a061c52a
Han Sae Jung <hansaejung@g.harvard.edu>
2019-04-16 03:43:56 -0400

    Update io.py

--- neuronal/io.py
+++ neuronal/io.py
@@ -1,1 +1,13 @@
 import os
+import numpy as np
+
+def loadtxt(filename):
+    """
+    Load txt data
+    
+    Parameters
+    ----------
+    filename : string
+        txt file
+    """
+  return np.loadtxt(filename)
