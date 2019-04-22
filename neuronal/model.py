0af4f150000ada93ae02ba4401783e794d4d9f3b
Han Sae Jung <hansaejung@g.harvard.edu>
2019-04-22 19:37:26 -0400

    added psp3_fit function to model.py

--- neuronal/model.py
+++ neuronal/model.py
@@ -65,3 +65,49 @@ def psp_fit(data, nsamples, initial_guess, plot=True, seed=None):
     if plot:
         pm.traceplot(trace)
     return trace
+
+def psp3_fit(x, y, nsamples, initial_guess, plot=True):
+    """
+    Uses pymc3 to calculate the trace for the PSP model. In this particular model, we assume three PSP peaks and
+    a constant baseline.
+    
+    Parameters
+    ----------
+    x : array
+        Input time data in seconds
+    y : array
+        Input voltage data in mV
+    nsamples : integer
+        Number of samples for pymc3 calculation
+    initial_guess : dictionary
+        Dictionary of initial guesses for the parameters of the model
+    plot : boolean
+        Plots of the marginal distributions of the estimated parameters (plotted when True)
+    """
+    with pm.Model() as PSP_model:
+        b = pm.Flat('b')
+        sigma = pm.HalfFlat('sigma')
+        a1 = pm.Flat('a1')
+        t1 = pm.Uniform('t1', lower=np.min(x), upper=np.max(x))
+        tau_d1 = pm.Uniform('tau_d1', lower=0, upper=0.1)
+        tau_r1 = pm.Uniform('tau_r1', lower=0, upper=0.1)
+        a2 = pm.Flat('a2')
+        t2 = pm.Uniform('t2', lower=np.min(x), upper=np.max(x))
+        tau_d2 = pm.Uniform('tau_d2', lower=0, upper=0.1)
+        tau_r2 = pm.Uniform('tau_r2', lower=0, upper=0.1)
+        a3 = pm.Flat('a3')
+        t3 = pm.Uniform('t3', lower=np.min(x), upper=np.max(x))
+        tau_d3 = pm.Uniform('tau_d3', lower=0, upper=0.1)
+        tau_r3 = pm.Uniform('tau_r3', lower=0, upper=0.1)
+        
+        model = (x >= t1) * a1 * (tt.exp(-(x-t1) / tau_d1) - tt.exp(-(x-t1) / tau_r1)) + b +\
+                (x >= t2) * a2 * (tt.exp(-(x-t2) / tau_d2) - tt.exp(-(x-t2) / tau_r2)) +\
+                (x >= t3) * a3 * (tt.exp(-(x-t3) / tau_d3) - tt.exp(-(x-t3) / tau_r3))
+        loglike = pm.Normal.dist(mu=model, sd=sigma).logp(y)
+        pm.Potential('result', loglike)
+        
+        trace=pm.sample(nsamples, cores=2, start=initial_guess)
+
+    if plot:
+        pm.traceplot(trace)
+    return trace
