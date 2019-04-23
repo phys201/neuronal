7ee2e1c3f93c684710e4bd550cf9aee6046d5b6b
Han Sae Jung <hansaejung@g.harvard.edu>
2019-04-23 02:38:33 -0400

    deleted psp3_fit

--- neuronal/__init__.py
+++ neuronal/__init__.py
@@ -1,3 +1,3 @@
-from .model import psp_log_likelihood, psp_fit, psp3_fit
+from .model import psp_log_likelihood, psp_fit
 from .io import NeuronalData, get_example_data_file_path, load_data
 from .plot import plot_fit
