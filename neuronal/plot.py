6ee36f288430306b868833b488f54f91ea8f9adf
Han Sae Jung <hansaejung@g.harvard.edu>
2019-04-22 22:22:17 -0400

    fixed plot_fit

--- neuronal/plot.py
+++ neuronal/plot.py
@@ -22,15 +22,15 @@ def plot_fit(data, sample, psp1=False, psp3=False):
     summary = pm.summary(sample)
     if psp1:
         b, a1, sigma, t1, tau_d1, tau_r1 = summary['mean']
-        PSP_model = (t >= t1) * a1 * (np.exp(-(t-t1) / tau_d1) - np.exp(-(t-t1) / tau_r1)) + b
+        model = (t >= t1) * a1 * (np.exp(-(t-t1) / tau_d1) - np.exp(-(t-t1) / tau_r1)) + b
     elif psp3:
         b, a1, a2, a3, sigma, t1, tau_d1, tau_r1, t2, tau_d2, tau_r2, t3, tau_d3, tau_r3 = summary['mean']
-        PSP_model = (t >= t1) * a1 * (tt.exp(-(t-t1) / tau_d1) - tt.exp(-(t-t1) / tau_r1)) + b +\
-                    (t >= t2) * a2 * (tt.exp(-(t-t2) / tau_d2) - tt.exp(-(t-t2) / tau_r2)) +\
-                    (t >= t3) * a3 * (tt.exp(-(t-t3) / tau_d3) - tt.exp(-(t-t3) / tau_r3))
+        model = (t >= t1) * a1 * (np.exp(-(t-t1) / tau_d1) - np.exp(-(t-t1) / tau_r1)) + b +\
+                    (t >= t2) * a2 * (np.exp(-(t-t2) / tau_d2) - np.exp(-(t-t2) / tau_r2)) +\
+                    (t >= t3) * a3 * (np.exp(-(t-t3) / tau_d3) - np.exp(-(t-t3) / tau_r3))
     plt.plot(t, v)
-    plt.plot(t, PSP_model, c='r')
-    plt.title('Estimated Fit from the PSP Model')
+    plt.plot(t, model, c='r')
+    plt.title('Estimated Fit from the Model')
     plt.xlabel('Time (s)')
     plt.ylabel('Voltage (mV)')
     plt.show()
