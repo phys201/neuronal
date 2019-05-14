ac1255cb1bd146d24ebc8eb861155d0c867d847d
Amelia Paine <apaine@g.harvard.edu>
2019-05-14 02:15:13 -0400

    Updated unit tests

--- neuronal/tests/test_analysis.py
+++ neuronal/tests/test_analysis.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from neuronal.io import get_example_data_file_path, NeuronalData
+from neuronal.io import get_example_data_file_path
 from neuronal.model import *
 from neuronal.analysis import *
 import matplotlib
@@ -25,9 +25,22 @@ class TestAnalysis(TestCase):
 
     def test_get_params(self):
         sigma, b_start, b_end, b, a, t_psp, tau_d, tau_r = get_params_from_df(data, sample)
+        self.assertAlmostEqual(sigma, 0.5251100153998798)
+        self.assertAlmostEqual(b_start, -30.372609326154524)
+        self.assertAlmostEqual(b_end, -30.36056833174976)
+        self.assertAlmostEqual(b[0], -30.398952468916747)
+        self.assertAlmostEqual(a[0], 0.2915479489155847)
+        self.assertAlmostEqual(t_psp[0], 451.4294192155971)
+        self.assertAlmostEqual(tau_d[0], 0.009691246075731536)
+        self.assertAlmostEqual(tau_r[0], 0.0009123478585051712)
 
     def test_peak_amplitudes(self):
         df = calculate_peak_amplitudes(0, sample, np.max(data.data['T']), data.num_psp)
+        self.assertTrue('peak_amp__0' in df.columns.values)
+        self.assertAlmostEqual(df['peak_amp__0'][0], 0.17820178467355885)
+        self.assertRaises(KeyError, calculate_peak_amplitudes, 1, sample, np.max(data.data['T']), data.num_psp)
 
     def test_report(self):
         value_minus, value_mid, value_plus = report_best_fit(get_quantiles(sample), 'b', print_fit=True)
+        self.assertAlmostEqual(value_minus, -30.46566738900276)
+        self.assertAlmostEqual(value_mid, -30.398952468916747)
